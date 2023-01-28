db2d57147edeefcc5c2515b035adc1f1764b2927
SpaikerZ <egori228337@gmail.com>
2023-01-28 14:17:02 +0300

    start_room.3

--- start_room.py
+++ start_room.py
@@ -5,35 +5,43 @@ import threading
 
 class start(object):
 
+	# this 'succes' global variavle. If player die or doing something wrong
+	# this wariable will be changed and make game was end
 	succes = True
 	def enter():
-		print(dedent("""
-			You slowly open your eyes. 
-			Something prickly metal on the neck.
-			Where I am? Why is it so dark?
-			I seem to have blood on my hands	
-		"""))
-		#time.sleep(10)
-		print('. . .')
-		time.sleep(1)
-		playsound('./audio/hello_my_dear(1).wav')
-		print('What???')
-		print('Where I am??!!')
-		time.sleep(1)
-		print('Help')
-		print(dedent("""
-			You have a key between your two legs.
-			Use it to open the collar.
-			You have 15 seconds
-		"""))	
-		playsound('./audio/open_collar_with_key(2).wav')	
-		
-		#asinc play music on the backgrounds
-		def _playSound():
-			playsound('./audio/SAW(3).mp3')
-		t1 = threading.Thread(target=_playSound)
-		t1.start()
+		while True:
+			print(dedent("""
+				You slowly open your eyes. 
+				Something prickly metal on the neck.
+				Where I am? Why is it so dark?
+				I seem to have blood on my hands	
+			"""))
+			#time.sleep(10)
+			print('. . .')
+			time.sleep(1)
+			#playsound('./audio/hello_my_dear(1).wav')
+			print('What???')
+			print('Where I am??!!')
+			time.sleep(1)
+			print('Help')
+			print(dedent("""
+				You have a key between your two legs.
+				Use it to open the collar.
+				You have 15 seconds
+			"""))	
+			#playsound('./audio/open_collar_with_key(2).wav')	
+			
+			#asinc play music on the backgrounds
+			def _playSound():
+				playsound('./audio/SAW(3).mp3')
+			t1 = threading.Thread(target=_playSound)
+			t1.start()
 
+			# this function is asinc and 
+			def _this_is_end():
+				
+
+			
 
 		print('WHAAAAAT?')
 
