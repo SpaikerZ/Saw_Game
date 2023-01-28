a06bfe5320df5eb9f7f141979670e67dfb56bdf6
SpaikerZ <egori228337@gmail.com>
2023-01-28 13:58:46 +0300

    start_room.2

--- start_room.py
+++ start_room.py
@@ -1,6 +1,7 @@
 import time 
 from textwrap import dedent
 from playsound import playsound
+import threading
 
 class start(object):
 
@@ -12,7 +13,7 @@ class start(object):
 			Where I am? Why is it so dark?
 			I seem to have blood on my hands	
 		"""))
-		time.sleep(10)
+		#time.sleep(10)
 		print('. . .')
 		time.sleep(1)
 		playsound('./audio/hello_my_dear(1).wav')
@@ -20,13 +21,23 @@ class start(object):
 		print('Where I am??!!')
 		time.sleep(1)
 		print('Help')
-		playsound('./audio/open_collar_with_key(2).wav')
 		print(dedent("""
-			You slowly open your eyes. 
-			Something prickly metal on the neck.
-			Where I am? Why is it so dark?
-			I seem to have blood on my hands	
-		"""))		
+			You have a key between your two legs.
+			Use it to open the collar.
+			You have 15 seconds
+		"""))	
+		playsound('./audio/open_collar_with_key(2).wav')	
+		
+		#asinc play music on the backgrounds
+		def _playSound():
+			playsound('./audio/SAW(3).mp3')
+		t1 = threading.Thread(target=_playSound)
+		t1.start()
+
+
+		print('WHAAAAAT?')
+
+		
 
 
 
