b206cfe023eff417b5af75b99abf191fa5c6996e
SpaikerZ <egori228337@gmail.com>
2023-01-29 15:03:02 +0300

    friend_room.2

--- friend_room.py
+++ friend_room.py
@@ -2,6 +2,7 @@ import time
 from playsound import playsound
 from textwrap import dedent
 from random import randint
+from sys import exit
 
 class friend(object):
 
@@ -12,45 +13,58 @@ class friend(object):
 		time.sleep(2)
 		print('. . .')
 		time.sleep(1)
-		playsound('./audio/ru/Scream.mp3')
+		#playsound('./audio/ru/Scream.mp3')
 		print('Spanch Bob?')
 		time.sleep(1)
 		print('He need a help')
-		playsound('./audio/ru/друг_приветствие.wav')
+		#playsound('./audio/ru/друг_приветствие.wav')
 		time.sleep(1)
 		print('Whata fu** is going on')
-		playsound('./audio/ru/Scream.mp3')
+		#playsound('./audio/ru/Scream.mp3')
 		print(dedent("""
 				Перед тобой лежат буквы
 				Собери из них слово
 				У тебя 15 секунд 
 				Если ты не соберешь слово правильно с первого раз твоего друга разорвет на части
 		"""))
-		playsound('./audio/ru/друг.wav')
+		#playsound('./audio/ru/друг.wav')
 		
 		print('Время пошло ...')
 		time_before = time.time()
-		
+	
 		true_word = 'Scream'
 		array_letters = ['S','c','r','e', 'a', 'm']
-		array_random_letters = 
-		str = ''
+		array_random_letters = []
+		string = ''
 		lenth = int(len(array_letters))
 		
 		for i in range(lenth):
-			a = randint(0, lenth)
+			new_lenth = int(len(array_letters) - 1)
+			a = randint(0, new_lenth)
 			item = array_letters[a]
+			array_random_letters.append(item)
+			array_letters.pop(a)
+		
+		string = '  '.join(array_random_letters)
+		print(string)
+		
+		word = str(input('[word]>'))
+		strip_word = word.strip()
+		
+		
+		if 'Scream' in strip_word:
+			time_after = time.time()
+			delta_time = int(time_after - timebefore)
+			if delta_time < 15:
+				print('...')
+				time.sleep(1)
+				print('*correct')		
+		else:
+			print('Yoy friend Die')
+			print('Game end')
+			exit(0)
 			
-		
-		
-		
-		# take random letter from array 
-		# append to array.random
-		# create str from array random
-		# print string
-		# give input 
-		# check input 
-		
+	
 		
 		
 		
